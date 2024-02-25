990b6ad18ddcbf9aa917c5fd4cb2a1f5c324fdb6
Daniel Tejada <dastte93@gmail.com>
2024-02-25 18:25:23 -0500

    estilos modificados

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -8,7 +8,7 @@ export default function GeneralLayout({
   return (
     <>
       <Navbar />
-      <main className="flex flex-col items-center p-24">
+      <main className="flex flex-col min-h-screen items-center p-24">
         <samp className="text-lg">Hola Mundo</samp>
         {children}
       </main>
